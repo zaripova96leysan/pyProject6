694d91a8458980921c05888655b76b6f6c07b5bf
user_l <zaripova_leysan@mail.ru>
2026-05-03 12:36:59 +0300

    fix: исправить отступ в sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,16 +2,15 @@ def filter_by_state(my_list, state='EXECUTED'):
     """Функция, которая принимает список словарей и возвращает отфильтрованный список"""
     result = []
 
-    for list in my_list:
-        if list.get('state') == state:
-            result.append(list)
+    for item in my_list:
+        if item.get('state') == state:
+            result.append(item)
 
     return result
 
 
 def sort_by_date(data, reverse=True):
     """Функция, принимающая список словарей и возвращает отсортированный по дате"""
-        return sorted(data, key=lambda x: x['date'], reverse=reverse)
-
+    return sorted(data, key=lambda x: x['date'], reverse=reverse)
 
 
